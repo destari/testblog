bb4866aaa87d9706898601c9ca1c4d8e81af7ef8
Eric Anderson <anderson@ttel.com>
2020-01-17 14:32:42 -0600

    rebuilding site Fri Jan 17 14:32:42 CST 2020

---
+++ content/posts/my-first-post.md
@@ -0,0 +1,7 @@
+---
+title: "My First Post"
+date: 2020-01-17T14:01:07-06:00
+draft: false
+---
+This is a small blog entry to test out Hugo.
+
--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,30 @@
 # If a command fails then the deploy stops
 set -e
 
+msg="rebuilding site $(date)"
+if [ -n "$*" ]; then
+	msg="$*"
+fi
+
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
+# Add changes to git.
+git add .
+
+# Commit changes.
+git commit -m "$msg"
+
+# pull to be fresh:
+git pull -r
+
+cd public
+
+git pull -r
+
+cd ..
+
 # Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+hugo -t ananke # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 # Go To Public folder
 cd public
@@ -15,10 +35,6 @@ cd public
 git add .
 
 # Commit changes.
-msg="rebuilding site $(date)"
-if [ -n "$*" ]; then
-	msg="$*"
-fi
 git commit -m "$msg"
 
 # Push source and build repos.
